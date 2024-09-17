8787c12c7a6176b6054b0e363ee0025b7a32cb88
tiago <dexe.me12@gmail.com>
2024-09-17 19:18:04 -0300

    praga

--- T1-pesticidas/main.pl
+++ T1-pesticidas/main.pl
@@ -1,43 +1,62 @@
 praga(pulgao).
-praga(cochonilha).
-praga(mosca_branca).
+praga(acaro_do_bronzeamento).
 praga(lagarta).
 praga(caracol).
-praga(lesma).
-praga(formiga_cortadeira).
-praga(bacteria).
-praga(fungo).
+praga(besouro).
+praga(ferrugem_do_buxo).
+praga(mofo).
+praga(oidio).
+praga(marcha_negra).
+praga(murcha_bacteriana).
+praga(fogo_bacteriano).
 
-caracteristica(pulgao, suga_seiva).
-caracteristica(pulgao, cor_verde).
-caracteristica(pulgao, secrecao_acucarada).
-caracteristica(cochonilha, cor_branca_ou_marrom).
-caracteristica(cochonilha, secrecao_acucarada).
-caracteristica(mosca_branca, cor_branca).
-caracteristica(mosca_branca, suga_seiva).
-caracteristica(lagarta, come_folhas).
-caracteristica(lagarta, deixa_folhas_picotadas).
-caracteristica(caracol, habito_noturno).
-caracteristica(lesma, habito_noturno).
-caracteristica(formiga_cortadeira, corta_folhas).
-caracteristica(bacteria, murchidao_vascular).
-caracteristica(bacteria, necrose).
-caracteristica(fungo, fumagina).
+caracteristica(pulgao, inseto_preto_ou_verde_esbranquiçado).
+caracteristica(pulgao, folhas_enroladas).
+caracteristica(pulgao, folhas_amareladas).
+caracteristica(acaro_do_bronzeamento, pequenos_aracnideos).
+caracteristica(acaro_do_bronzeamento, folhas_murchas).
+caracteristica(acaro_do_bronzeamento, folhas_marrons).
+caracteristica(lagarta, corpo_cilindrico_verde).
+caracteristica(lagarta, folhas_picotadas).
+caracteristica(caracol, moluscos_com_casco).
+caracteristica(caracol, folhas_esburacadas_com_limo).
+caracteristica(besouro, insetos_de_casco_escuro).
+caracteristica(besouro, pequenos_furos_nas_folhas).
+caracteristica(ferrugem_do_buxo, circulos_alaranjados_nas_folhas).
+caracteristica(mofo, folhas_murchas).
+caracteristica(mofo, folhas_marrons).
+caracteristica(oidio, manchas_esbranquiçadas_nas_folhas).
+caracteristica(marcha_negra, manchas_negras_arredondadas_nas_folhas).
+caracteristica(murcha_bacteriana, caule_podre_e_mole).
+caracteristica(fogo_bacteriano, folhas_queimadas).
+caracteristica(fogo_bacteriano, folhas_marrons).
 
-tratamento(pulgao, solucao_cebola_agua_alho).
-tratamento(cochonilha, solucao_cebola_agua_alho).
-tratamento(mosca_branca, mistura_detergente_oleo).
-tratamento(lagarta, pesticida).
-tratamento(caracol, pesticida).
-tratamento(lesma, pesticida).
-tratamento(formiga_cortadeira, pesticida).
-tratamento(bacteria, bactericida).
-tratamento(fungo, fungicida).
+% Tratamentos orgânicos
+tratamento(pulgao, organico, mistura_agua_detergente_neutro).
+tratamento(lagarta, organico, mistura_cebola_alho_agua).
+tratamento(caracol, organico, bicarbonato_sodio_solo).
+tratamento(besouro, organico, solucao_agua_ervas_aromaticas).
+tratamento(ferrugem_do_buxo, organico, oleo_neem).
+tratamento(mofo, organico, extratos_aquosos_gengibre).
+tratamento(oidio, organico, mistura_bicarbonato_sodio_agua).
 
-identificar_praga(suga_seiva, pulgao).
-identificar_praga(secrecao_acucarada, cochonilha).
-identificar_praga(cor_branca, mosca_branca).
-identificar_praga(come_folhas, lagarta).
-identificar_praga(habito_noturno, caracol).
-identificar_praga(murchidao_vascular, bacteria).
-identificar_praga(fumagina, fungo).
+% Tratamentos químicos
+tratamento(acaro_do_bronzeamento, quimico, biopesticida_oximatrine).
+tratamento(fogo_bacteriano, quimico, biopesticida_fago).
+
+% Tratamentos culturais
+tratamento(mancha_negra, cultural, poda_ramos_contaminados).
+tratamento(murcha_bacteriana, cultural, poda_ramos_contaminados).
+
+
+identificar_praga(inseto_preto_ou_verde_esbranquiçado, pulgao).
+identificar_praga(folhas_enroladas, pulgao).
+identificar_praga(pequenos_aracnideos, acaro_do_bronzeamento).
+identificar_praga(corpo_cilindrico_verde, lagarta).
+identificar_praga(moluscos_com_casco, caracol).
+identificar_praga(insetos_de_casco_escuro, besouro).
+identificar_praga(circulos_alaranjados_nas_folhas, ferrugem_do_buxo).
+identificar_praga(manchas_esbranquiçadas_nas_folhas, oidio).
+identificar_praga(manchas_negras_arredondadas_nas_folhas, marcha_negra).
+identificar_praga(caule_podre_e_mole, murcha_bacteriana).
+identificar_praga(folhas_queimadas, fogo_bacteriano).
